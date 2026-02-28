ca9bd1b3b8e9dc8af47bc1195905cbd7221f775e
Utsav sachan <sachanutsav800@gmail.com>
2026-02-28 21:52:34 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter sdecond number "))
+print("Addition: ",a+b)
+print("Substraction: ",a-b)
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
---
+++ task2.py
@@ -0,0 +1,4 @@
+a = input("Enter First Name: ")
+b = input("Enter Second Name: ")
+c = a+" "+b
+print(f"Hello, {c}! Welcome to the Pyhton program.")
